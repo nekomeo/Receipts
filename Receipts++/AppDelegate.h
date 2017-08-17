e937782b0e97491ea670e0af41cacecbcb27cba7
nekomeo <linh.tu545@gmail.com>
2017-08-17 11:19:38 -0700

    Initial commit

---
+++ Receipts++/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Receipts++
+//
+//  Created by Elle Ti on 2017-08-17.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Receipts++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts++
+//
+//  Created by Elle Ti on 2017-08-17.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
